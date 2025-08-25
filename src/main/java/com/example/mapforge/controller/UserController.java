536f2751a365aca6aafa965f8bcb572d3d7dd744
eduardalexandrut <aeduardtoni@gmail.com>
2025-08-25 20:44:01 +0200

    Added PUT endpoint for users

--- src/main/java/com/example/mapforge/controller/UserController.java
+++ src/main/java/com/example/mapforge/controller/UserController.java
@@ -2,10 +2,9 @@ package com.example.mapforge.controller;
 
 import com.example.mapforge.model.entity.User;
 import com.example.mapforge.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mapforge.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,19 +14,30 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository,  UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
-    // GET /api/v1/users → all users
+    // GET /api/v1/users -> all users
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    // GET /api/v1/users/{id} → single user by id
+    // GET /api/v1/users/{id} -> single user by id
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Integer id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    // PUT /api/v1/users/{id} -> update a user
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User user) {
+        return userService.updateUser(id, user)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/example/mapforge/service/UserService.java
+++ src/main/java/com/example/mapforge/service/UserService.java
@@ -3,9 +3,11 @@ package com.example.mapforge.service;
 import com.example.mapforge.model.dto.AuthResponseDTO;
 import com.example.mapforge.model.entity.User;
 import com.example.mapforge.repository.UserRepository;
+import com.google.googlejavaformat.Op;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -22,6 +24,7 @@ public class UserService {
         this.jwtService = jwtService;
     }
 
+    //TODO password checks (length, strength, etc...)
     public AuthResponseDTO signUp(User user) {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             return new AuthResponseDTO(false, "User already exists");
@@ -44,5 +47,37 @@ public class UserService {
 
         String token = jwtService.generateToken(user.get());
         return new AuthResponseDTO(true, "User signed in successfully", token);
+    }
+
+    public Optional<User> updateUser(Integer id, User updatedUser) {
+        Optional<User> oldUser = userRepository.findById(id);
+
+        if (oldUser.isEmpty()) {
+            return Optional.empty();
+        }
+
+        //Only certain fields are updatable
+        oldUser.get().setUsername(
+                Optional.ofNullable(updatedUser.getUsername()).orElse(oldUser.get().getUsername())
+        );
+        oldUser.get().setName(
+                Optional.ofNullable(updatedUser.getName()).orElse(oldUser.get().getName())
+        );
+        oldUser.get().setSurname(
+                Optional.ofNullable(updatedUser.getSurname()).orElse(oldUser.get().getSurname())
+        );
+        oldUser.get().setDateOfBirth(
+                Optional.ofNullable(updatedUser.getDateOfBirth()).orElse(oldUser.get().getDateOfBirth())
+        );
+        oldUser.get().setEmail(
+                Optional.ofNullable(updatedUser.getEmail()).orElse(oldUser.get().getEmail())
+        );
+
+        //TODO password checks (length, strength, etc...)
+        if (Optional.ofNullable(updatedUser.getPassword()).isPresent() && !updatedUser.getPassword().isEmpty()) {
+            oldUser.get().setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+
+        return Optional.of(userRepository.save(oldUser.get()));
     }
 }
